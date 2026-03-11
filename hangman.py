daa557f7eacfee43cc20f778f0b12905ced3415f
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-11 17:33:11 +0100

    update/game

--- hangman.py
+++ hangman.py
@@ -1,49 +1,60 @@
 import random
 
 words = [
-    "developer"
-    # "python",
-    # "programming",
-    # "developer",
-    # "algorithm",
-    # "function",
-    # "variable",
-    # "computer",
-    # "keyboard",
-    # "software",
-    # "database",
+    "python",
+    "programming",
+    "developer",
+    "algorithm",
+    "function",
+    "variable",
+    "computer",
+    "keyboard",
+    "software",
+    "database",
 ]
 
-# Set number of attempts
-attempts = 10
 
-# Select a random word from list
-word = random.choice(words)
-guessed_letters = set()
-lines = ["_" for char in word]
-print(" ".join(lines))
+def run_game():
+    while True:
+        # Set number of attempts
+        attempts = 10
 
-while attempts > 0 and "_" in lines:
-    answer = input("Guess a letter: ").lower()
+        # Select a random word from list
+        word = random.choice(words)
+        guessed_letters = set()
+        lines = ["_" for char in word]
+        print(" ".join(lines))
 
-    if answer in guessed_letters:
-        print("Already guessed that letter")
-        continue
+        # Game functionality
+        while attempts > 0 and "_" in lines:
+            answer = input("Guess a letter: ").lower()
 
-    guessed_letters.add(answer)
+            if answer in guessed_letters:
+                print("Already guessed that letter")
+                continue
 
-    if answer in word:
-        for i, char in enumerate(word):
-            if char == answer:
-                lines[i] = answer
-        print("Correct!")
-    else:
-        attempts -= 1
-        print(f"Wrong guess: {attempts} attempts remaining.")
+            guessed_letters.add(answer)
 
-    print(" ".join(lines))
+            if answer in word:
+                for i, char in enumerate(word):
+                    if char == answer:
+                        lines[i] = answer
+                print("Correct!")
+            else:
+                attempts -= 1
+                print(f"Wrong guess: {attempts} attempts remaining.")
 
-if not "_" in lines:
-    print("You won!")
-else:
-    print(f"Game over! The correct word was {word}")
+            print(" ".join(lines))
+
+        if not "_" in lines:
+            print("You won!")
+        else:
+            print(f"Game over! The correct word was {word}")
+
+        again = input("Would you like to play again (y/n): ")
+        if again.lower() not in ["y", "yes"]:
+            print("Thanks for playing!")
+            break
+
+
+run_game()
